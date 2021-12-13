e651d1347933bc8e235a700aad64e652992f7b4c
maeek <maciej@suchanecki.me>
2021-12-13 16:09:49 +0100

    fix: Shared types package reference

--- production/types/src/example/index.d.ts
+++ production/types/src/example/index.d.ts
@@ -1,5 +1,10 @@
+import { HttpStatus } from "@nestjs/common";
+
 export namespace ExampleService {
   export interface ServiceResponse {
-    data: number; 
+    status: HttpStatus;
+    message: string;
+    errors?: { [key: string | number ]: string };
+    result: number; 
   }
 }
